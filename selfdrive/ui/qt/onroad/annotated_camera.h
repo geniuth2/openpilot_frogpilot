d8f53c9ddd1c3a1d80cb1bde44b225d2e81cde97
geniuth <geniuth@gmail.com>
2024-12-06 21:54:08 +0900

    NDA 추가

--- selfdrive/ui/qt/onroad/annotated_camera.h
+++ selfdrive/ui/qt/onroad/annotated_camera.h
@@ -182,6 +182,9 @@ private:
   inline QColor yellowColor(int alpha = 255) { return QColor(255, 255, 0, alpha); }
 
 protected:
+  // NDA neokii
+  void drawRoadLimitSpeed(QPainter &p);
+
   void paintGL() override;
   void initializeGL() override;
   void showEvent(QShowEvent *event) override;
