c90ae5ee7f53d30de2676244d6ffdb0a53c19be9
geniuth <geniuth@gmail.com>
2025-01-29 00:10:41 +0900

    Error 수정

--- selfdrive/ui/soundd.py
+++ selfdrive/ui/soundd.py
@@ -59,8 +59,7 @@ sound_list: dict[int, tuple[str, int | None, float]] = {
   AudibleAlert.thisIsFine: ("this_is_fine.wav", 1, MAX_VOLUME),
   AudibleAlert.uwu: ("uwu.wav", 1, MAX_VOLUME),
 
-  # Other
-  AudibleAlert.goat: ("goat.wav", None, MAX_VOLUME),
+  # NDA
   AudibleAlert.speedDown: ("speeddown.wav", 3, MAX_VOLUME),
 }
 
