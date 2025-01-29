0c05e28e0edb760ed207dc1119592dc8b06c477e
geniuth <geniuth@gmail.com>
2025-01-29 23:12:39 +0900

    Bolt 페달 수정  - 저속에서 급가속 방지코드 추가 From 양민  - 의미 없는 new api 세팅 값 변경

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -293,7 +293,9 @@ class CarInterface(CarInterfaceBase):
         ret.flags |= GMFlags.PEDAL_LONG.value
         ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_PEDAL_LONG
         # Note: Low speed, stop and go not tested. Should be fairly smooth on highway
-        if use_new_api:
+        if use_new_api: #no meaning
+          ret.longitudinalTuning.kpBP = [0., 3., 6., 35.]
+          ret.longitudinalTuning.kpV = [0.08, 0.175, 0.225, 0.33]
           ret.longitudinalTuning.kiBP = [0., 35.0]
           ret.longitudinalTuning.kiV = [0.05, 0.05]
         else:
